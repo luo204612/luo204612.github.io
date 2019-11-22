b3708c2755043c0bdbc4973096b12817525aac58
unknown <lhm204612@163.com>
2019-11-22 12:30:56 +0800

    p5

--- app/js/sketch.js
+++ app/js/sketch.js
@@ -1,50 +1,26 @@
-var ox, oy, nx, ny, spd, spd0, spd00, spd00, spd000, spd0000,weight;
-
+var ox, oy, nx, ny
+var off = true
 function pen() {
-    var minWeight = 2;
-    var maxWeight = 30;
-    var weightIncrement = 4;
     nx = mouseX;
     ny = mouseY;
-
-    spd = dist(ox, oy, nx, ny);
-    avgSpd = (spd + spd0 + spd00 + spd000 + spd0000) / 5;
-    if (between2(avgSpd, 1, 2)) { weight += (weightIncrement * 2) }
-    if (between2(avgSpd, 3, 5)) { weight += (weightIncrement) }
-    if (between2(avgSpd, 6, 40)) { weight -= weightIncrement }
-    if (between2(avgSpd, 61, 999)) { weight -= (weightIncrement * 2) }
-    if (avgSpd < 1) { weight += 1 }
-    weight = Math.max(weight, minWeight);
-    weight = Math.min(weight, maxWeight);
-
     stroke(0);
-    strokeWeight(weight)
-    line(ox, oy, nx, ny);
+    strokeWeight(10)
+    console.log((nx-ox)*(nx-ox)+(ny-oy)*(ny-oy))
+    if ((nx-ox)*(nx-ox)+(ny-oy)*(ny-oy)>1000)
+        line(nx, ny, nx, ny);
+    else
+        line(ox, oy, nx, ny)
     ox = nx;
     oy = ny;
-
-    spd0000 = spd000;
-    spd000 = spd00;
-    spd00 = spd0;
-    sped0 = spd;
-
 }
-
-function between2(num, a, b) {
-    return (num >= a && num <= b);
-}
-
-
 
 function setup() {
-    createCanvas(300, 500);
+    createCanvas(350, 600);
     background(255);
 }
 
 function draw() {
     document.body.addEventListener("touchstart", () => {
-        ox = mouseX;
-        oy = mouseY;
         document.body.addEventListener("touchmove", pen)
     })
     document.body.addEventListener("touchend", () => {
